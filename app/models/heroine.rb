430bf1b52c82e3dab5a6ad014760f3b7f73be445
Ben Yellin <byellindc@gmail.com>
2019-02-07 11:09:38 -0500

    initial power show/index views

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,6 +1,6 @@
 class Heroine < ApplicationRecord
   belongs_to :power
-  delegate :name, :description, to: :power, prefix: true
+  delegate :name, :display_name, :description, to: :power, prefix: true
 
   validates :name, presence: true
   validates :super_name, presence: true
--- app/models/power.rb
+++ app/models/power.rb
@@ -2,4 +2,8 @@ class Power < ApplicationRecord
   has_many :heroines
 
   validates :name, presence: true
+
+  def display_name
+    self.name.titleize
+  end
 end
