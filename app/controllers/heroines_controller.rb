5a02b8ab18a29483230eeb51f8bfe44cc19391ae
shannon-nabors <shannon.nabors@gmail.com>
2019-02-07 10:47:51 -0500

    added form to create a new superheroine

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -7,5 +7,20 @@ class HeroinesController < ApplicationController
   def show
     @heroine = Heroine.find(params[:id])
   end
-  
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    heroine = Heroine.create(heroine_params)
+    redirect_to heroine_path(heroine)
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
