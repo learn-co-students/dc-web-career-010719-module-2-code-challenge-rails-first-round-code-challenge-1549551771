1ff5703d4f840f3fee688f39641ccb9cd3199acb
Ben Yellin <byellindc@gmail.com>
2019-02-07 10:21:10 -0500

    create controller actions

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,27 @@
 class HeroinesController < ApplicationController
+
+  before_action :get_heroine, only: %i(show)
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private 
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
   end
 end
