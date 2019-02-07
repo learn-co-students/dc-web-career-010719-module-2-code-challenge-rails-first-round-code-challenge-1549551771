fc1685bbb445b448754f4626987c879b65ddea2f
Ben Yellin <byellindc@gmail.com>
2019-02-07 10:40:17 -0500

    add model validation

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,4 +1,7 @@
 class Heroine < ApplicationRecord
   belongs_to :power
   delegate :name, :description, to: :power, prefix: true
+
+  validates :name, presence: true
+  validates :super_name, presence: true
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,5 @@
 class Power < ApplicationRecord
   has_many :heroines
+
+  validates :name, presence: true
 end
